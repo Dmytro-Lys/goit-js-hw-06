fa1a6c78957ca15e7fab1156a2c1b2f8cfd79563
Dmytro-Lys <d.lysenko122@gmail.com>
2023-05-02 00:45:25 +0300

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const elemIngredients = [];
+const listIngredients = document.querySelector('#ingredients');
+ingredients.forEach(item => {
+  elemIngredients.push(document.createElement("li"));
+  elemIngredients[elemIngredients.length - 1].textContent = item;
+  elemIngredients[elemIngredients.length - 1].classList.add("item");
+})
+listIngredients.append(...elemIngredients);
