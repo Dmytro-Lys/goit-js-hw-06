603b1839332ea10c8ff7d7e8189957081e07642e
Dmytro-Lys <d.lysenko122@gmail.com>
2023-05-06 23:06:12 +0300

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,19 @@
+const refs = {
+  body: document.querySelector("body"),
+  btn: document.querySelector(".change-color"),
+  spColor: document.querySelector(".color")
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeBgColor = () => {
+  const bgColor = getRandomHexColor();
+  refs.body.style.backgroundColor = bgColor;
+  refs.spColor.textContent = bgColor;
+}
+
+refs.btn.addEventListener("click", changeBgColor);
