b40079e4d938f24677c1248d67c89fefbcf25fde
Dmytro-Lys <d.lysenko122@gmail.com>
2023-05-06 18:21:36 +0300

    task-06

---
+++ js/task-06.js
@@ -0,0 +1,12 @@
+const inputField = document.querySelector("#validation-input");
+const inputLength = inputField.getAttribute("data-length");
+
+const addValidation = () => inputField.value.length == inputLength ? inputField.classList.add("valid") : inputField.classList.add("invalid");
+
+const removeValidation = () => {
+    if (inputField.classList.contains("valid"))  inputField.classList.remove("valid");
+    if (inputField.classList.contains("invalid"))  inputField.classList.remove("invalid");
+}
+
+inputField.addEventListener("blur", addValidation);
+inputField.addEventListener("focus", removeValidation);
