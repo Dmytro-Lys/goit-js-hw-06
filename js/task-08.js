3a5585a794fea2b1b853e4e1ea5517db197be44a
Dmytro-Lys <d.lysenko122@gmail.com>
2023-05-06 22:46:08 +0300

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,11 @@
+const loginForm = document.querySelector(".login-form");
 
+const submitForm = (e) => {
+    e.preventDefault();
+    const {email , password} = e.currentTarget.elements;
+    if (email.value === "" || password.value === "")  return alert("Please fill in all the fields!");
+    console.log({email: email.value, password: password.value });
+    e.currentTarget.reset();
+}
+
+loginForm.addEventListener("submit", submitForm);
